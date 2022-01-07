1d4b05d2a71dc3460c789709cc0868e4d74b8005
hosu <hosulee11@gmail.com>
2022-01-07 11:50:17 -0800

    style: bestsellers mobile view layout

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const openMenuButton = document.querySelector('.mobile-nav-header .bars');
 const closeMenuButton = document.querySelector('.mobile-nav .fa-times');
 
+//side menu
 openMenuButton.addEventListener('click', function (e) {
   document.querySelector('.mobile-nav').style.transform = 'translate(0)';
 });
